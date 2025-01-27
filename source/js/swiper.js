2699a463fe4d164875fec8530321990b6cd4d5a9
unknown <farid.gai0in@gmail.com>
2025-01-27 14:38:33 +0600

    FC

---
+++ source/js/swiper.js
@@ -0,0 +1,43 @@
+function initSwiperBullet() {
+  const swiper = new Swiper('.swiper-bullet', {
+    speed: 450,
+    effect: 'coverflow',
+    rewind: true,
+    pagination: {
+      el: '.bullet-pagination',
+      clickable: true,
+    },
+    
+    updateOnWindowResize: true,
+    spaceBetween: 20,
+    breakpoints: {
+      200: {
+        slideToClickedSlide: true,
+        coverflowEffect: {
+          rotate: 60,
+          stretch: 14,
+          depth: 500,
+          scale: 0.7,
+          modifier: 1,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      },
+      768: {
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 0,
+          modifier: 0,
+          slideShadows: false,
+        },
+        allowTouchMove: false,
+        spaceBetween: 0,
+      },
+    },
+  })
+}
+
+$(document).ready(function () {
+  initSwiperBullet()
+})
